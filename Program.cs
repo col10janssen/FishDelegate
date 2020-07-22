5bb5557c9bab7e6b4e537403e55c89a697b49eae
Colten <cjanssen@Coltens-MacBook-Pro.local>
2020-07-22 17:50:16 -0600

    Create delegate for Spawning a tuna, implement egg interfaces

--- Program.cs
+++ Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
 
-namespace FishDelegate
+public interface IFish
 {
-    class Program
+    Egg Lay();
+}
+
+public class Egg
+{
+    private IFish FishToHatch { get; set; }
+
+    public Egg(Func<IFish> fishToHatch)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        FishToHatch = fishToHatch();
+    }
+
+    public IFish Hatch()
+    {
+        return FishToHatch;
+    }
+}
+
+public class Tuna : IFish
+{
+    public Egg Lay()
+    {
+        return new Egg(SpawnTuna);
+    }
+
+    private static IFish SpawnTuna()
+    {
+        return new Tuna();
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var fish = new Tuna();
+        var egg = fish.Lay();
+        var smallFish = egg.Hatch();
     }
 }
